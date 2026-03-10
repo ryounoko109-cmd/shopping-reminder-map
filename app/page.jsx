7f630b1f4a842d3bb9979c7901892f108784b8a2
りょう <ryounoko109@gmail.com>
2026-03-10 20:32:42 +0900

    update page

--- app/page.jsx
+++ app/page.jsx
@@ -325,7 +325,7 @@ export default function MapPage() {
 
       {/* Map */}
       <div style={{ height: "calc(100vh - 52px)", position: "relative" ,zIndex: 0}}>
-        <MapContainer center={currentPos || [35.6812, 139.7671]} zoom={16} style={{ height: "100%",zIndex: 0}}tap={false}>
+        <MapContainer center={currentPos || [35.6812, 139.7671]} zoom={16} style={{ height: "100%",zIndex: -1}}tap={false}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           <MapJump target={jumpTarget} />
           <ChangeCursor isAdding={isAdding} />
